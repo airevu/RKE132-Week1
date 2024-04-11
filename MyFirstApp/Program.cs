5dbaa8e5a6c43e6917934c58b8d3cc7e4d3ff631
Aire Vuchmann <airevuchmann@Aires-MacBook-Air.local>
2024-04-11 22:36:49 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+
+//rakendus küsib kasutajalt sisestada tema nime
+//rakenudus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string - sõne
+String userName = Console.Readline(); //Input
+
+Console.WriteLine("Hello"+ ", " + userName + "!"); //Output
+//string interpolation
+
+Console.WriteLine($"Hello, {userName} !"); //Output
